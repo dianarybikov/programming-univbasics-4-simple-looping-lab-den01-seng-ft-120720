79b5596fd687a1aec508fb43ae9249a2340f1f57
Diana Rybikov <diana.ryb214@gmail.com>
2020-11-20 19:06:32 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,7 +26,7 @@ def return_string_array(array)
   count = 0
  new_array = []
  while count < array.length do
-  puts new_array << (array[count].to_s)
+   new_array << array[count].to_s
   count += 1
 end
 end
